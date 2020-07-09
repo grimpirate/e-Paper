f1ea4e0f1333cf84e17f3482d9966ed91c1e0762
Fernando Sanchez <grimpirate_jrs@yahoo.com>
2020-07-09 19:21:54 -0400

    bmp functionality only

--- examples/EPD_2in13b_V2_test.c
+++ examples/EPD_2in13b_V2_test.c
@@ -57,21 +57,7 @@ int EPD_2in13b_V2_test(void)
     Paint_NewImage(BlackImage, EPD_2IN13B_V2_WIDTH, EPD_2IN13B_V2_HEIGHT, 270, WHITE);
     Paint_NewImage(RYImage, EPD_2IN13B_V2_WIDTH, EPD_2IN13B_V2_HEIGHT, 270, WHITE);
 
-    //Select Image
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-    Paint_SelectImage(RYImage);
-    Paint_Clear(WHITE);
-
-#if 1   // show bmp
-    printf("show window BMP-----------------\r\n");
-    Paint_SelectImage(BlackImage);
-    GUI_ReadBmp("./pic/100x100.bmp", 10, 0);
-    Paint_SelectImage(RYImage);
-    Paint_Clear(WHITE);    
-    EPD_2IN13B_V2_Display(BlackImage, RYImage);
-    DEV_Delay_ms(2000);
-
+    // show bmp
     printf("show red bmp------------------------\r\n");
     Paint_SelectImage(BlackImage);
     GUI_ReadBmp("./pic/2in13bc-b.bmp", 0, 0);
@@ -79,51 +65,6 @@ int EPD_2in13b_V2_test(void)
     GUI_ReadBmp("./pic/2in13bc-ry.bmp", 0, 0);
     EPD_2IN13B_V2_Display(BlackImage, RYImage);
     // DEV_Delay_ms(2000);
-
-#endif    
-    
-#if 1   // show image for array    
-    printf("show image for array\r\n");
-//    EPD_2IN13B_V2_Display(gImage_2in13b_b, gImage_2in13b_r);
-
-    EPD_2IN13B_V2_Display(gImage_2in13c_b, gImage_2in13c_y);
-    DEV_Delay_ms(2000);
-#endif
-
-#if 1   // Drawing on the image
-    /*Horizontal screen*/
-    //1.Draw black image
-    printf("Draw black image\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-    Paint_DrawPoint(5, 70, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 80, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-    Paint_DrawLine(20, 70, 50, 100, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawLine(50, 70, 20, 100, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawRectangle(60, 70, 90, 100, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawCircle(125, 85, 15, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawString_CN(5, 15, "ÄãºÃabc", &Font12CN, WHITE, BLACK);
-
-    //2.Draw red image
-    printf("Draw red image\r\n");
-    Paint_SelectImage(RYImage);
-    Paint_Clear(WHITE);
-    Paint_DrawPoint(5, 90, RED, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 100, RED, DOT_PIXEL_4X4, DOT_STYLE_DFT);
-    Paint_DrawLine(125, 70, 125, 100, RED, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine(110, 85, 140, 85, RED, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawRectangle(20, 70, 50, 100, RED, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(165, 85, 15, RED, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawString_EN(5, 0, "waveshare Electronics", &Font12, BLACK, WHITE);
-    Paint_DrawNum(5, 50, 987654321, &Font16, WHITE, RED);
-
-    printf("EPD_Display\r\n");
-    EPD_2IN13B_V2_Display(BlackImage, RYImage);
-    DEV_Delay_ms(2000);
-#endif
-
-    printf("Clear...\r\n");
-    EPD_2IN13B_V2_Clear();
 
     printf("Goto Sleep...\r\n");
     EPD_2IN13B_V2_Sleep();
