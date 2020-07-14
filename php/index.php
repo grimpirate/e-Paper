85fb612c042b25b7c5651abc890ce3cffae3c179
grimpirate <grimpirate_jrs@yahoo.com>
2020-07-14 15:28:41 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -5,10 +5,12 @@ Flight::route('/', function(){
     Flight::render('main');
     $foo = array_map(function($ele){
     return pack('C',$ele);
-}, array_fill(0, 104/8*212, 0));
+}, array_merge(array_fill(0, 104/8*212/2, 0xff),array_fill(0, 104/8*212/2, 0x00)));
 
-file_put_contents('../black', implode('', $foo));
+file_put_contents('black', implode('', $foo));
+while(!file_exists('black')) usleep(500);
     exec('../epd');
+    unlink('black');
 });
 
 Flight::start();
