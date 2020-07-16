7f458432cc56161f26454e82306ca59c4cec0a33
grimpirate <grimpirate_jrs@yahoo.com>
2020-07-16 14:36:19 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -12,12 +12,18 @@ Flight::route('/', function(){
 		return pack('C',$ele);
 	}, array_merge(array_fill(0, 104/8*212/2, 0xff),array_fill(0, 104/8*212/2, 0x00)));
 	file_put_contents('black', implode('', $foo));*/
-	$img = Image::fromFile("test.png");
-	file_put_contents('black', $img->toBlackAndWhite()->getRasterData());
 	
-	while(!file_exists('black')) usleep(500);
+	$img = Image::fromFile("test.png")->toBlackAndWhite();
+	file_put_contents('red', $img->getRasterData());
+	$img->invert();
+	file_put_contents('black', $img->getRasterData());
+	
+	while(!file_exists('black') || !file_exists('red')) usleep(500);
+	
 	exec('../epd');
+	
 	unlink('black');
+	unlink('red');
 });
 
 Flight::start();
