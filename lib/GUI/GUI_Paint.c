8ad9a4711d3870c51cea738086a5998d9db17700
grimpirate <grimpirate_jrs@yahoo.com>
2020-07-18 09:35:56 -0400

    Update GUI_Paint.c

--- lib/GUI/GUI_Paint.c
+++ lib/GUI/GUI_Paint.c
@@ -68,11 +68,11 @@
 ******************************************************************************/
 #include "GUI_Paint.h"
 #include "DEV_Config.h"
-/*#include "Debug.h"
+#include "Debug.h"
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h> //memset()
-#include <math.h>*/
+#include <math.h>
 
 /******************************************************************************
 function: Draw Pixels
